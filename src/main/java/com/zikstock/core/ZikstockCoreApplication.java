0003663b92d6aec99a9dac6fca66841659656323
Emmanuel Legros <emmanuel.legros@gmail.com>
2020-12-14 11:08:33 +0100

    Init empty app

---
+++ src/main/java/com/zikstock/core/ZikstockCoreApplication.java
@@ -0,0 +1,13 @@
+package com.zikstock.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZikstockCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZikstockCoreApplication.class, args);
+	}
+
+}
