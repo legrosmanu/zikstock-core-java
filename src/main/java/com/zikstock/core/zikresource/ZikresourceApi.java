847cd8c5f242e53ae71e67e0a277d46b4dbb735f
Emmanuel Legros <emmanuel.legros@gmail.com>
2020-12-16 22:58:33 +0100

    Validation exception handler

---
+++ src/main/java/com/zikstock/core/zikresource/ZikresourceApi.java
@@ -0,0 +1,37 @@
+package com.zikstock.core.zikresource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/api")
+public class ZikresourceApi {
+
+    @Autowired
+    private ZikresourceRepository zikresourceRepository;
+
+    @PostMapping("/zikresources")
+    public Zikresource createZikresource(@Valid @RequestBody Zikresource zikresource) {
+        return this.zikresourceRepository.save(zikresource);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String errorMessage = error.getDefaultMessage();
+            errors.put("code", "400-1"); // Check if error.getObjectName() == zikresource when needed?
+            errors.put("details", errorMessage);
+        });
+        return errors;
+    }
+
+}
