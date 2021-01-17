f18217d39f29570e16dabc708a951c76123a7cab
Emmanuel Legros <emmanuel.legros@gmail.com>
2021-01-17 21:25:43 +0100

    Before to stop the Java version

---
+++ src/test/java/com/zikstock/core/zikresource/ZikresourceApiTests.java
@@ -0,0 +1,14 @@
+package com.zikstock.core.zikresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ZikresourceApiTests {
+
+    @Test
+    public void PostShouldCreateZikResource() {
+        
+    }
+    
+}
